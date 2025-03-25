0aaf2479fe6c8bc288bc3a81b1e0a20f10e75b3c
Pedro-RGS <pedro.gsouza@upe.br>
2025-03-25 15:41:57 -0300

    Validation has been added in UserCreationDto.java

--- src/main/java/br/upe/desafio_picpay/controller/dto/UserCreationDto.java
+++ src/main/java/br/upe/desafio_picpay/controller/dto/UserCreationDto.java
@@ -1,11 +1,17 @@
 package br.upe.desafio_picpay.controller.dto;
 
 import br.upe.desafio_picpay.infra.enums.UserTypeEnum;
+import jakarta.validation.constraints.NotNull;
 
 public record UserCreationDto (
+        @NotNull(message = "The user's CPF cannot be blank")
         String cpf,
+        @NotNull(message = "The user's email cannot be blank")
         String email,
+        @NotNull(message = "The user need a type")
         UserTypeEnum userType,
+        @NotNull(message = "The user's name cannot be blank")
         String completeName,
+        @NotNull(message = "The user's password cannot be blank")
         String password){
 }
